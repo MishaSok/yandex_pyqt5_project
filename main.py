a09d80543113627b4f6a6948af2d0e31a2b2d214
mishasok <S347614s347614>
2021-10-25 15:17:04 +0200

    - added LoginForm class

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
-
-class Example(QWidget):
+class LoginForm(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.setWindowTitle('test')
+        uic.loadUi('login_form.ui', self)
 
     def initUI(self):
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Четвёртая программа')
+        self.setGeometry(300, 400, 300, 400)
+        self.setFixedSize(300, 300)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = LoginForm()
     ex.show()
     sys.exit(app.exec())
